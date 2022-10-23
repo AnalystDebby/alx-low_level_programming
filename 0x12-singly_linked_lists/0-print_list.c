bf63d6d19a183ae22f94184845d12918caa3daee
deborahihesiaba <deborahihesiaba@gmail.com>
2022-10-23 09:04:53 +0100

    Added files

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - function that prints all the element
+ * @h: pointer
+ * Return: nodes
+*/
+size_t print_list(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
